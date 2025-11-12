e595b42497d3f83c3d015545c154c97f1d5bd62c
Rennel24 <24-00424@g.batstate-u.edu.ph>
2025-11-12 19:55:00 +0800

    add Program.java main runner for system execution

---
+++ model/Program.java
@@ -0,0 +1,17 @@
+package model;
+public class Program {
+
+    private String programName;
+
+    public Program(String programName) {
+        this.programName = programName;
+    }
+
+    public String getProgramName() {
+        return this.programName;
+    }
+
+    public void setProgramName(String programName) {
+        this.programName = programName;
+    }
+}
