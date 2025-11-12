631c42c216f51661e8c17ea275cb54324a613857
Marmhar <salaparejhonmhar@gmail.com>
2025-11-12 17:16:08 +0800

    feat: add Main class as the program entry point for Campus-HUB

---
+++ main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Runner runner = new Runner();
+        runner.run();
+
+        System.out.println("Thank you for using Schedule Management System!");
+    }
+}
